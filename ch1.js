642b935f8e44834e3f400511404ee10e85e9054f
Pakhii10 <pakhisolanki0@gmail.com>
2026-05-28 18:42:14 +0530

    Added JS lessons

---
+++ ch1.js
@@ -0,0 +1,26 @@
+// printing hellow world on console
+console.log("pakhi")
+
+// intoduction to var let and const
+
+var x=10 
+var x=20 //allowed
+if(x==20){
+    x=5 // allowed
+}
+console.log(x)
+
+let a=20
+a=5 // allowed
+// let x=10 Not Allowed
+if(true){
+    x=15 // not accessible outside the scope
+}
+console.log(a)
+
+const b=10 //cannot redeclare
+if(true){
+    
+    console.log(b)
+}
+//pakhi
---
+++ ch2.js
@@ -0,0 +1,16 @@
+// introduction to data types in js
+
+let name="pakhi" //string
+let age=20 //int 2**53
+//bigint
+let logged=true //boolean
+let state=null //null
+let state1;//undefined
+//symbol used in reat components 
+//object examples
+sd
+console.log(typeof "Pakhi")
+console.log(typeof state)
+console.log(typeof state1)
+
+//pakhi
